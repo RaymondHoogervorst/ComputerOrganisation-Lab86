8bfb7a4c7acdbecaa6f0f1621479106a413a2f0a
Raymond Hoogervorst <raymond.hoogervorst4@gmail.com>
2021-02-22 23:46:29 +0100

    Completed inout.s

--- inout.s
+++ inout.s
@@ -1,15 +1,52 @@
-.text
-outputstr: .asciz "Raymond Hoogervorst: 2691516\nErik Vunš: 2696857\n"
+.data
+outputstr: .asciz "Assignment 2: inout\n"
+inputformat: .asciz "%d"
+outputformat: .asciz "%d\n"
 
 .global main
 
+.text
+# ***********************************************************
+# * Subroutine : inout                                      *
+# * Description: asks for user input and returns increment  *
+# ***********************************************************
+inout:
+   pushq %rbp;             #storing old base pointer
+   movq %rsp, %rbp         #setting new base pointer
+
+   subq $8, %rsp           #setting parameters for scanf
+   leaq -8(%rbp), %rsi
+   movq $inputformat, %rdi
+   movq $0, %rax
+   pushq %rbp;             #stack preparation for scanf (not included in subroutine itself)
+   movq %rsp, %rbp         
+   call scanf              #calling scanf
+   movq %rbp, %rsp         #stack restoring for scanf (not included in subroutine itself)
+   popq %rbp                   
+
+   popq %rsi               #computing output value
+   incq %rsi
+
+   movq $0, %rax           #printing output
+   movq $outputformat, %rdi
+   call printf
+
+   movq %rbp, %rsp         #placing back stack pointer
+   popq %rbp               #placing back base pointer      
+   ret
+
+
 main: #entry point
-   pushq %rbp        #setup base pointer
-   movq %rsp, %rbp 
+   pushq %rbp              #storing old base pointer
+   movq %rsp, %rbp         #setting new base pointer
 
    movq $0, %rax           #no vector registers used
    movq $outputstr, %rdi   #set adress of string
-   call printf             #call subroutine
+   call printf             #call subroutine to display message
+
+   call inout              #calling selfmade function
+
+
 end:
    mov $0, %rdi
    call exit
