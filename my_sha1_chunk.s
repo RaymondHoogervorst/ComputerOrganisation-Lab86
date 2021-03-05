c78811c3970e0603d276f372f63d6f7b6b0aefaf
Raymond Hoogervorst <raymond.hoogervorst4@gmail.com>
2021-03-05 12:32:51 +0100

    Created loops

--- my_sha1_chunk.s
+++ my_sha1_chunk.s
@@ -6,18 +6,53 @@
 # %r8 = f                           #
 # %r9 = k                           #
 # %r10 to %r14 = a to e             #
-# %r15 = temp                       # 
+# %rax = temp values                #
 # **********************************#
 
 sha1_chunk:
 
-   # first loop: extend words array
+   # extend words to 80
+   movq $16, %rdx
+   extensionloop:
 
+      incq %rdx
+      cmpq $79, %rdx
+      jle extensionloop
 
    # initialize hash value
 
 
    # main loop
+   movq $0, %rdx
+   mainloop:
+      # determine quarter
+      cmpq %rdx, $19
+      jle first
+      cmpq %rdx, $39
+      jle second
+      cmpq %rdx, $59
+      jle third
+      cmpq %rdx, $19
+      jle fourth
+      jmp endloop
+
+      first:
+
+         jmp repeat
+      second:
+
+         jmp repeat
+      third:
+
+         jmp repeat
+      fourth:
+
+         jmp repeat
+
+      repeat:
+         incq %rdx
+         jmp mainloop
+   endloop:
 
 
    # Add hash results
